11e2bc094329f4d4e024d5807481729f7fcaf2d6
Roy Weller <mail@royweller.co.uk>
2018-03-04 17:12:43 +0000

    Adding valid parenthesis classes

---
+++ Code/Problems/Algorithms/Easy/ValidParentheses.cs
@@ -0,0 +1,10 @@
+﻿namespace Raw.LeetCode.Code.Problems.Algorithms.Easy
+{
+    public static class ValidParentheses
+    {
+        public static bool Solution(string s)
+        {
+            return false;
+        }
+    }
+}
---
+++ Test/Problems/Algorithms/Easy/ValidParanthesesTests.cs
@@ -0,0 +1,16 @@
+﻿namespace Raw.LeetCode.Test.Problems.Algorithms.Easy
+{
+    using NUnit.Framework;
+    using Code.Problems.Algorithms.Easy;
+
+
+    [TestFixture]
+    class ValidParanthesesTests
+    {
+        [TestCase("{[()]}", ExpectedResult = true)]
+        public bool TestSolution(string s)
+        {
+            return ValidParentheses.Solution(s);
+        }
+    }
+}
