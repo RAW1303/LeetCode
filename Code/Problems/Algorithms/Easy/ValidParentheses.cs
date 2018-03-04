a4d6c1f599fa2b77d187fced75d825a4fe6e8a01
Roy Weller <mail@royweller.co.uk>
2018-03-04 20:32:07 +0000

    Complete ValidParentheses problem

--- Code/Problems/Algorithms/Easy/ValidParentheses.cs
+++ Code/Problems/Algorithms/Easy/ValidParentheses.cs
@@ -1,10 +1,37 @@
 ﻿namespace Raw.LeetCode.Code.Problems.Algorithms.Easy
 {
+    using System.Collections.Generic;
+
     public static class ValidParentheses
     {
         public static bool Solution(string s)
         {
-            return false;
+            Stack<char> openingParentheses = new Stack<char>();
+            Dictionary<char, char> parenthesesPairs = new Dictionary<char, char>
+            {
+                { ')', '(' },
+                { '}', '{' },
+                { ']', '[' }
+            };
+
+            foreach (char c in s)
+            {
+                if(parenthesesPairs.ContainsKey(c))
+                {
+                    char lastOpening;
+                    bool popped = openingParentheses.TryPop(out lastOpening);
+                    if (!popped || lastOpening != parenthesesPairs[c])
+                    {
+                        return false;
+                    }
+                }
+                else if (parenthesesPairs.ContainsValue(c))
+                {
+                    openingParentheses.Push(c);
+                }
+            }
+
+            return openingParentheses.Count == 0;
         }
     }
 }
--- Test/Problems/Algorithms/Easy/ValidParanthesesTests.cs
+++ Test/Problems/Algorithms/Easy/ValidParanthesesTests.cs
@@ -7,7 +7,13 @@
     [TestFixture]
     class ValidParanthesesTests
     {
-        [TestCase("{[()]}", ExpectedResult = true)]
+        [TestCase("()", ExpectedResult = true, TestName = "Basic")]
+        [TestCase("{[()]}", ExpectedResult = true, TestName = "Nested")]
+        [TestCase("()[]{}", ExpectedResult = true, TestName = "Sequence")]
+        [TestCase("(]", ExpectedResult = false, TestName = "Wrong closing")]
+        [TestCase("([)]", ExpectedResult = false, TestName = "Wrong order")]
+        [TestCase("()[]{}]", ExpectedResult = false, TestName = "Too many closing")]
+        [TestCase("()[]{", ExpectedResult = false, TestName = "Too few closing")]
         public bool TestSolution(string s)
         {
             return ValidParentheses.Solution(s);
