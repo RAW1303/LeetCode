ec8e9566c162c6f1bb5b8e7cd44df8e3f8bc66ca
Roy Weller <mail@royweller.co.uk>
2018-03-04 20:32:35 +0000

    Start Merge Two Sorted Lists problem

---
+++ Code/Problems/Algorithms/Easy/MergeTwoSortedLists.cs
@@ -0,0 +1,67 @@
+﻿namespace Raw.LeetCode.Code.Problems.Algorithms.Easy
+{
+    using System;
+    using System.Collections.Generic;
+
+    public static class MergeTwoSortedLists
+    {
+        public static ListNode Solution(ListNode l1, ListNode l2)
+        {
+            ListNode newListNodeRoot = new ListNode(int.MinValue);
+            ListNode newListNodeCurrent = newListNodeRoot;
+
+            while (l1 != null || l2 != null)
+            {
+                if(l1 != null && (l2 == null || l1.val < l2.val))
+                {
+                    newListNodeCurrent.next = new ListNode(l1.val);
+                    l1 = l1.next;
+                }
+                else
+                {
+                    newListNodeCurrent.next = new ListNode(l2.val);
+                    l2 = l2.next;
+                }
+
+                newListNodeCurrent = newListNodeCurrent.next;
+            }
+
+            return newListNodeRoot.next;
+        }
+    }
+
+
+    public class ListNode
+    {
+        public int val;
+
+        public ListNode next;
+
+        public ListNode(int x) { val = x; }
+
+        public ListNode(int[] intArray)
+        {
+            ListNode previousNode = this;
+            for (int i = 1; i < intArray.Length; i++)
+            {
+                ListNode currentNode = new ListNode(intArray[i]);
+                previousNode.next = currentNode;
+                previousNode = currentNode;
+            }
+        }
+
+        public int[] ToArray()
+        {
+            List<int> list = new List<int>();
+            ListNode currentNode = this;
+
+            while(currentNode != null)
+            {
+                list.Add(currentNode.val);
+                currentNode = currentNode.next;
+            }
+
+            return list.ToArray();
+        }
+    }
+}
---
+++ Test/Problems/Algorithms/Easy/MergeTwoSortedListsTests.cs
@@ -0,0 +1,18 @@
+﻿namespace Raw.LeetCode.Test.Problems.Algorithms.Easy
+{
+    using System;
+    using NUnit.Framework;
+    using Code.Problems.Algorithms.Easy;
+
+    [TestFixture]
+    class MergeTwoSortedListsTests
+    {
+        [TestCase(new int[] { 1,2,4 }, new int[] { 1, 3, 4 }, new int[] { 1, 1, 2, 3, 4, 4 })]
+        public void TestSolution(int[] l1, int[] l2, int[] expected)
+        {
+            ListNode node1 = new ListNode(l1);
+            ListNode node2 = new ListNode(l2);
+            Assert.AreEqual(expected, MergeTwoSortedLists.Solution(node1, node2).ToArray());
+        }
+    }
+}
