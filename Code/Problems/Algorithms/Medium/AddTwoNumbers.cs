db0239327fea057c4c9cecb6507b1c3d71508237
Roy Weller <mail@royweller.co.uk>
2018-03-06 23:36:37 +0000

    Complete add two numbers problem

---
+++ Code/Problems/Algorithms/Medium/AddTwoNumbers.cs
@@ -0,0 +1,30 @@
+﻿namespace Raw.LeetCode.Code.Problems.Algorithms.Medium
+{
+    using Raw.LeetCode.Code.Utilities;
+
+    public static class AddTwoNumbers
+    {
+        public static ListNode Solution(ListNode l1, ListNode l2)
+        {
+            ListNode fullSum = new ListNode(-1);
+            ListNode currentNode = fullSum;
+            int carry = 0;
+
+            while(l1 != null || l2 != null || carry > 0)
+            {
+                int l1Val = l1 != null ? l1.val : 0;
+                int l2Val = l2 != null ? l2.val : 0;
+                int sum = l1Val + l2Val + carry;
+
+                carry = sum / 10;
+                currentNode.next = new ListNode(sum % 10);
+
+                l1 = l1?.next;
+                l2 = l2?.next;
+                currentNode = currentNode.next;
+            }
+
+            return fullSum.next;
+        }
+    }
+}
---
+++ Test/Problems/Algorithms/Medium/AddTwoNumbersTest.cs
@@ -0,0 +1,19 @@
+﻿namespace Raw.LeetCode.Test.Problems.Algorithms.Medium
+{
+    using NUnit.Framework;
+    using Code.Problems.Algorithms.Medium;
+    using Code.Utilities;
+
+    class AddTwoNumbersTest
+    {
+        [TestCase(new int[] { 2, 4, 3 }, new int[] { 5, 6, 4 }, ExpectedResult = new int[] { 7, 0, 8 }, TestName = "Basic")]
+        [TestCase(new int[] { 0, 0, 5 }, new int[] { 0, 0, 5 }, ExpectedResult = new int[] { 0, 0, 0, 1 }, TestName = "Carry")]
+        [TestCase(new int[] { 0, 0, 5 }, new int[] { 0, 5 }, ExpectedResult = new int[] { 0, 5, 5 }, TestName = "Different lengths")]
+        public int[] TestSolution(int[] l1, int[] l2)
+        {
+            ListNode node1 = new ListNode(l1);
+            ListNode node2 = new ListNode(l2);
+            return AddTwoNumbers.Solution(node1, node2).ToArray();
+        }
+    }
+}
