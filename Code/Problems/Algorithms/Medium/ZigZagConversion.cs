13e54a2ac31a21c6f4f0b4ed0fde1e06e7999d39
Roy Weller <mail@royweller.co.uk>
2018-03-10 01:33:03 +0000

    Completed ZigZag Conversion

---
+++ Code/Problems/Algorithms/Medium/ZigZagConversion.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Raw.LeetCode.Code.Problems.Algorithms.Medium
+{
+    public static class ZigZagConversion
+    {
+        public static string Solution(string s, int numRows)
+        {
+            StringBuilder[] rows = new StringBuilder[numRows];
+            
+            int direction = 1;
+            int nextRow = 0;
+
+            foreach(char c in s)
+            {
+                if(rows[nextRow] == null)
+                {
+                    rows[nextRow] = new StringBuilder(c.ToString());
+                }
+                else
+                {
+                    rows[nextRow].Append(c);
+                }
+
+                if(numRows > 1)
+                    nextRow += direction;
+
+                if (nextRow == 0 || nextRow == numRows - 1)
+                    direction = direction * -1;
+            }
+
+            for (int i = 1; i < numRows; i++)
+            {
+                rows[0].Append(rows[i]);
+            }
+
+            return rows[0].ToString();
+        }
+    }
+}
---
+++ Test/Problems/Algorithms/Medium/ZigZagConversionTest.cs
@@ -0,0 +1,19 @@
+﻿namespace Raw.LeetCode.Test.Problems.Algorithms.Medium
+{
+    using NUnit.Framework;
+    using Code.Problems.Algorithms.Medium;
+
+    [TestFixture]
+    class ZigZagConversionTest
+    {
+        [TestCase("PAYPALISHIRING", 3, ExpectedResult = "PAHNAPLSIIGYIR", TestName = "Basic")]
+        [TestCase("0123456789", 1, ExpectedResult = "0123456789", TestName = "1 Row")]
+        [TestCase("0123456789", 2, ExpectedResult = "0246813579", TestName = "2 Rows")]
+        [TestCase("0123456789", 3, ExpectedResult = "0481357926", TestName = "3 Rows")]
+        [TestCase("0123456789", 4, ExpectedResult = "0615724839", TestName = "4 Rows")]
+        public string TestSolution(string s, int rows)
+        {
+            return ZigZagConversion.Solution(s, rows);
+        }
+    }
+}
