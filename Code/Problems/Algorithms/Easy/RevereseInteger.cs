c458764ea27eeb44004ca4760c5ed42311c0bd0b
Roy Weller <mail@royweller.co.uk>
2018-03-15 23:36:25 +0000

    7. Reverse Integer (Complete)

---
+++ Code/Problems/Algorithms/Easy/RevereseInteger.cs
@@ -0,0 +1,30 @@
+﻿namespace Raw.LeetCode.Code.Problems.Algorithms.Easy
+{
+    using System;
+
+    public static class RevereseInteger
+    {
+        public static int Solution(int x)
+        {
+            int result = 0;
+            string xString = x.ToString();
+            char[] resultString = new char[xString.Length];
+            int currentIndex = xString.Length - 1;
+            foreach(char c in xString)
+            {
+                if(c == '-')
+                {
+                    resultString[0] = c;
+                }
+                else
+                {
+                    resultString[currentIndex--] = c;
+                }
+            }
+
+            int.TryParse(new string(resultString), out result);
+
+            return result;
+        }
+    }
+}
---
+++ Test/Problems/Algorithms/Easy/ReverseIntegerTest.cs
@@ -0,0 +1,19 @@
+﻿namespace Raw.LeetCode.Test.Problems.Algorithms.Easy
+{
+    using NUnit.Framework;
+    using Code.Problems.Algorithms.Easy;
+
+    [TestFixture]
+    class ReverseIntegerTest
+    {
+        [TestCase(123, ExpectedResult = 321, TestName = "Basic")]
+        [TestCase(-123, ExpectedResult = -321, TestName = "BasicNegative")]
+        [TestCase(120, ExpectedResult = 21, TestName = "LeadingZero")]
+        [TestCase(2147483647, ExpectedResult = 0, TestName = "PositiveOverflow")]
+        [TestCase(-2147483645, ExpectedResult = 0, TestName = "NegativeOverflow")]
+        public int TestSolution(int x)
+        {
+            return RevereseInteger.Solution(x);
+        }
+    }
+}
