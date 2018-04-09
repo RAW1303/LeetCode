9a165c11b1adce9d3e2d7ace496260b00fcca2c1
Roy Weller <mail@royweller.co.uk>
2018-04-10 00:38:15 +0100

    Adding #811 Subdomain Visit Count

---
+++ Code/Problems/Algorithms/Easy/SubdomainVisitCount.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raw.LeetCode.Code.Problems.Algorithms.Easy
+{
+    public static class SubdomainVisitCount
+    {
+        public static IList<string> Solution(string[] cpdomains)
+        {
+            IDictionary<string, int> subdomains = new Dictionary<string, int>();
+            IList<string> result = new List<string>();
+
+            foreach(string cpdomain in cpdomains)
+            {
+                string fullDomain = string.Empty;
+                string[] mapping = cpdomain.Split(' ');
+                string[] domains = mapping[1].Split('.');
+                int count = int.Parse(mapping[0]);
+                for(int i = domains.Length - 1; i >= 0; i--)
+                {
+                    fullDomain = domains[i] + fullDomain;
+                    if(!subdomains.ContainsKey(fullDomain))
+                    {
+                        subdomains.Add(fullDomain, count);
+                    }
+                    else
+                    {
+                        subdomains[fullDomain] = subdomains[fullDomain] + count;
+                    }
+
+                    fullDomain = '.' + fullDomain;
+                }
+            }
+            
+            foreach(string key in subdomains.Keys)
+            {
+                result.Add(string.Format("{0} {1}", subdomains[key], key));
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Test/Problems/Algorithms/Easy/SubdomainVisitCountTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Raw.LeetCode.Code.Problems.Algorithms.Easy;
+
+namespace Raw.LeetCode.Test.Problems.Algorithms.Easy
+{
+    class SubdomainVisitCountTest
+    {
+        [TestCase(new string[] { "9001 discuss.leetcode.com" }, new string[] { "9001 discuss.leetcode.com", "9001 leetcode.com", "9001 com" }, TestName = "Basic")]
+        [TestCase(new string[] { "900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org" }, new string[] { "901 mail.com", "50 yahoo.com", "900 google.mail.com", "5 wiki.org", "5 org", "1 intel.mail.com", "951 com" }, TestName = "Second")]
+        public void TestSolution(string[] cpdomains, IList<string> expectedArray)
+        {
+            IList<string> actual = SubdomainVisitCount.Solution(cpdomains);
+            actual = actual.OrderBy(o => o).ToList();
+            IList<string> expected = expectedArray.OrderBy(o => o).ToList();
+            
+            Assert.AreEqual(expected, actual); 
+        }
+    }
+}
