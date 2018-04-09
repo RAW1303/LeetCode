18b9b1ff35402730da841ef93faba0e8932d4b53
Roy Weller <mail@royweller.co.uk>
2018-04-10 00:38:37 +0100

    Adding #5 Longest Palindromic Substring

---
+++ Code/Problems/Algorithms/Medium/LongestPalindromicSubstring.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raw.LeetCode.Code.Problems.Algorithms.Medium
+{
+    public class LongestPalindromicSubstring
+    {
+        public static string Solution(string s)
+        {
+            int start = 0, length = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                int len1 = ExpandAroundCenter(s, i, i);
+                int len2 = ExpandAroundCenter(s, i, i + 1);
+                int len = Math.Max(len1, len2);
+                if (len > length)
+                {
+                    start = i - (len - 1) / 2;
+                    length = len;
+                }
+            }
+            return s.Substring(start, length);
+        }
+
+        private static int ExpandAroundCenter(String s, int left, int right)
+        {
+            int L = left, R = right;
+            while (L >= 0 && R < s.Length && s[L] == s[R])
+            {
+                L--;
+                R++;
+            }
+            return R - L - 1;
+        }
+    }
+}
---
+++ Test/Problems/Algorithms/Medium/LongestPalindromicSubstringTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using Raw.LeetCode.Code.Problems.Algorithms.Medium;
+
+namespace Raw.LeetCode.Test.Problems.Algorithms.Medium
+{
+    class LongestPalindromicSubstringTest
+    {
+        [TestCase("babad", new string[] { "bab", "aba" }, TestName = "Basic")]
+        [TestCase("cbbd", new string[] { "bb" }, TestName = "Short")]
+        public void TestSolution(string s, string[] valid)
+        {
+            Assert.Contains(LongestPalindromicSubstring.Solution(s), valid);
+        }
+    }
+}
