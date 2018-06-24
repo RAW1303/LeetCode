077e747791f440fbbf3fc4770849ee34c5ffdf5a
Roy Weller <mail@royweller.co.uk>
2018-06-24 19:56:07 +0100

    9: Palindrome number - all tests passing

---
+++ Code/Problems/Algorithms/Easy/PalindromeNumber.cs
@@ -0,0 +1,40 @@
+﻿namespace Raw.LeetCode.Code.Problems.Algorithms.Easy
+{
+    using System;
+
+    public class PalindromeNumber
+    {
+        public static bool Solution(int x)
+        {
+            return UsingInts(x);
+        }
+
+        private static bool UsingStrings(int x)
+        {
+            if (x < 0)
+                return false;
+
+            int reverese = RevereseInteger.Solution(x);
+            return x == reverese;
+        }
+
+        private static bool UsingInts(int x)
+        {
+            bool result = x >= 0;
+
+            int power = (int)Math.Log10(x);
+            int loops = (power + 2) / 2;
+            int i = 0;
+
+            while (result && i < loops)
+            {
+                int frontEnd = (x / (int)Math.Pow(10, power - i)) % 10;
+                int backEnd = (x / (int)Math.Pow(10, i)) % 10;
+                result = frontEnd == backEnd;
+                i++;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Test/Problems/Algorithms/Easy/PalindromeNumberTest.cs
@@ -0,0 +1,20 @@
+﻿namespace Raw.LeetCode.Test.Problems.Algorithms.Easy
+{
+    using NUnit.Framework;
+    using Code.Problems.Algorithms.Easy;
+
+    [TestFixture]
+    class PalindromeNumberTest
+    {
+        [TestCase(121, ExpectedResult = true, TestName = "Basic true")]
+        [TestCase(10, ExpectedResult = false, TestName = "Basic false")]
+        [TestCase(0, ExpectedResult = true, TestName = "Zero")]
+        [TestCase(-121, ExpectedResult = false, TestName = "Negative")]
+        [TestCase(int.MaxValue, ExpectedResult = false, TestName = "Max")]
+        [TestCase(int.MinValue, ExpectedResult = false, TestName = "Min")]
+        public bool TestSolution(int x)
+        {
+            return PalindromeNumber.Solution(x);
+        }
+    }
+}
